5030a429bc918f9e1720cbf60ba3d2b878620862
Flavio Pinheiro <flavio.couto@fiocruz.br>
2024-12-13 15:40:57 -0300

    Criação do menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar arquivo");
+            Console.WriteLine("0 - Sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Exit(); break;
+                case 1: Open(); break;
+                case 2: Create(); break;
+                default: Menu(); break;
+            }
+
+            static void Open()
+            {
+                Console.WriteLine("Abrir");
+                Menu();
+            }
+
+            static void Create()
+            {
+                Console.WriteLine("Criar");
+                Menu();
+            }
+
+            static void Exit()
+            {
+                Console.Clear();
+                Console.WriteLine("Saindo...");
+                System.Environment.Exit(0);
+            }
+
+        }
+
+    }
+}
